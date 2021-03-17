3419ce50552d2927babdc3ceaa2b8e6ae9cec91c
Afikur Rahman <me.afikur@gmail.com>
2021-03-17 21:16:45 +0600

    initial commit: spring mvc java configuration

---
+++ src/main/java/com/afikur/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.afikur.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHome() {
+        System.out.println("hello");
+        return "home";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: afikur
+  Date: 3/17/2021
+  Time: 8:32 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <h1>Welcome to My Homepage!</h1>
+</body>
+</html>
