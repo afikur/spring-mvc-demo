f6162532c994650ec73e1dd10757a7e9fcf527b3
Afikur Rahman <me.afikur@gmail.com>
2021-03-17 21:43:57 +0600

    use getMapping instead of RequestMapping

--- src/main/java/com/afikur/controller/HomeController.java
+++ src/main/java/com/afikur/controller/HomeController.java
@@ -2,13 +2,11 @@ package com.afikur.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String getHome() {
         System.out.println("hello");
         return "home";
